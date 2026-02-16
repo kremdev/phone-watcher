023eac5f6a8bb53b631007f0c68169c630d779a5
Krem <128897310+kremdev@users.noreply.github.com>
2026-02-16 07:10:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
+// تفعيل CORS لكل المواقع
 app.use(cors());
+
+// لو بدك تحدد دومين معين فقط:
+// app.use(cors({ origin: "https://example.com" }));
+
 app.use(express.json({ limit: "50mb" }));
 
 let latestPage = null;
@@ -26,4 +31,3 @@ app.get("/latest", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running...");
 });
-
